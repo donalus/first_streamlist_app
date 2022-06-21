1c9cc614148f8b87ab3eb1ddc67a6d7f6eea798d
Donal Heidenblad <donal.h@gmail.com>
2022-06-21 09:45:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Hello Streamlit! Why doesn\'t this tutorial include the requisite \'hello world\'?')
+
+streamlit.header('Oh, I guess they are making a point?')
+
+streamlit.text('I hope streamlit has a nicer way of making pages using templates because this would be onerous')
