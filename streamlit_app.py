02b5032d0fadde6ba53a4e36ad9816186d6bef12
Donal Heidenblad <donal.h@gmail.com>
2022-06-22 13:45:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
-add_my_fruit = streamlit.input("What fruit would you like to add?")
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 if add_my_fruit is not null:
   streamlit.text(f"Thanks for adding {add_my_fruit}")
